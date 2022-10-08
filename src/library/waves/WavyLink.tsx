ee47e01b7015c59de594ab6abf7948d0afb1352b
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-10-08 10:59:53 -0600

    WavyLink ++

---
+++ src/library/waves/WavyLink.tsx
@@ -0,0 +1,49 @@
+import { FC, MouseEvent, ReactNode } from "react";
+import { createRoot } from "react-dom/client";
+import { useNavigate } from "react-router-dom";
+
+//! CREATE LATER
+// import { WavyWaves } from "./WavyWaves";
+
+export type WavyLinkProps = {
+  to: string;
+  children: ReactNode;
+  color?: string;
+};
+
+export const WavyLink: FC<WavyLinkProps> = ({ to, children, color }) => {
+  const navigate = useNavigate();
+
+  const handleClick = (e: MouseEvent<HTMLButtonElement> | undefined) => {
+    e?.preventDefault();
+
+    if (!document.getElementById("react-wavy-transitions__waves")) {
+      // change the url in address bar
+      window.history.pushState("", "", to);
+
+      // get access to wave container
+      const container = createRoot(
+        document.getElementById("react-wavy-transitions__container")!
+      );
+
+      // show the waves
+      container.render(<WavyWaves color={color} />);
+
+      // do the route change
+      setTimeout(() => navigate(to), 750); // half total animation
+
+      // hide the waves
+      setTimeout(() => container.unmount(), 1500); // total animation
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      className="react-wavy-transitions__wavy-link"
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  );
+};
