e9c679334abe6e580fe68ab81ac18bfe7cfae209
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-10-08 18:26:06 -0600

    update package.json /waves

---
+++ src/library/waves/index.d.ts
@@ -0,0 +1,10 @@
+import {ReactNode} from 'react';
+
+export type WavyLinkProps = {
+    to:string;
+    children: 'ReactNode';
+    color?: string;
+};
+
+export { WavyContainer } from './WavyContainer';
+export { WavyLink } from './WavyLink'
