a7b0c375c68dab8f8573760d9ed0e254831778dc
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-10-10 07:54:50 -0600

    ++

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,3 +1,20 @@
-const Wrapper = () => <>Contact</>;
+import * as React from 'react';
+import {render} from 'react-dom';
+import {motion} from 'framer-motion';
+import './styles.css';
+
+
+const Wrapper = () => {
+    return(
+        <div className='example-container'>
+            <motion.div>
+                
+            </motion.div>
+
+        </div>
+    )
+}
 
 export default Wrapper;
+
+
